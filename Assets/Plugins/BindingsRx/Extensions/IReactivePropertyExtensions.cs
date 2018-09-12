c079f9cfb1bc92e9547e30ae3c9cf0284a84af04
Patrick Lavoie <patrick.lavoie00@gmail.com>
2018-09-11 23:36:01 -0400

    Added plugin BindingsRx.

---
+++ Assets/Plugins/BindingsRx/Extensions/IReactivePropertyExtensions.cs
@@ -0,0 +1,10 @@
+﻿using UniRx;
+
+namespace BindingsRx.Extensions
+{
+    public static class IReactivePropertyExtensions
+    {
+        public static ReadOnlyReactiveProperty<string> ToTextualProperty<T>(this IReactiveProperty<T> nonStringProperty)
+        { return nonStringProperty.Select(x => x.ToString()).ToReadOnlyReactiveProperty(); }
+    }
+}
